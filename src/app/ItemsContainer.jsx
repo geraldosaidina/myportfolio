4f54dca395a6a3553463ecb6a419f8a4bd16ce4f
geraldosaidina <gerarantes99@gmail.com>
2024-10-02 00:56:50 +0200

    final changes

--- src/app/ItemsContainer.jsx
+++ src/app/ItemsContainer.jsx
@@ -1,5 +1,5 @@
 import Item from "./Item";
-import { PRODUCTS, RESOURCES, COMPANY, SUPPORT } from "./Menus";
+import {  SUPPORT } from "./Menus";
 const ItemsContainer = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16">
